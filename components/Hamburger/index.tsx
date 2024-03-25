a8a8c4ca66a49959e6d2ee75189583967a3bc5a8
kchsu <kevin3117213@gmail.com>
2024-03-25 22:24:06 +0800

    Modify: hamburger slices' translate animation distance

--- components/Hamburger/index.tsx
+++ components/Hamburger/index.tsx
@@ -1,8 +1,8 @@
 export default function Hamburger({ isOpen }: { isOpen: boolean }) {
-    const slice1 = isOpen ? "translate-y-[6px] rotate-45" : "translate-y-0 rotate-0";
+    const slice1 = isOpen ? "translate-y-2 rotate-45" : "translate-y-0 rotate-0";
     const slice2 = isOpen ? "-translate-x-full opacity-0" : "translate-x-0 opacity-100";
-    const slice3 = isOpen ? "-translate-y-[14px] -rotate-45" : "translate-y-0 rotate-0";
-    
+    const slice3 = isOpen ? "-translate-y-3 -rotate-45" : "translate-y-0 rotate-0";
+
     return (
         <>
             <div className={slice1 + " slice"}></div>
