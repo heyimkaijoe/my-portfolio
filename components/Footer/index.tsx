81b2e5e92ef77f990cadb1a443dce26b654ca14e
kchsu <kevin3117213@gmail.com>
2024-03-28 18:00:50 +0800

    Add: footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,10 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 export default function Footer() {
     return (
-        <div className="">
-            <div className="social-links">
-                {/* LinkedIn and Github */}
+        <footer className="bg-prheart-200 mt-20 py-4 text-prheart-950">
+            <div className="flex justify-center gap-4 mb-2">
+                <a href="https://github.com/heyimkaijoe">
+                    <FontAwesomeIcon icon={faGithub} size="xl" />
+                </a>
+                <a href="https://www.linkedin.com/in/kchsutwtpe/">
+                    <FontAwesomeIcon icon={faLinkedin} size="xl" />
+                </a>
             </div>
-            <div className="all-rights"></div>
-        </div>
+
+            © 2024 Kevin Hsu. All rights reserved.
+        </footer>
     );
 }
