722eea2384bd58c176f1c4ed449ace3aab6abc33
kchsu <kevin3117213@gmail.com>
2024-03-28 19:16:17 +0800

    Add: theme toggle w/o dark-theme classes

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,27 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Hamburger from '../Hamburger';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
     const [burgerOpen, setBurgerOpen] = useState(false);
     const toggleBurger = () => setBurgerOpen(!burgerOpen);
 
+    type Theme = null | "dark" | "light";
+    const [theme, setTheme] = useState<Theme>(null);
+    const isDarkTheme = theme === "dark";
+    const handleThemeToggle = () => setTheme(isDarkTheme ? "light" : "dark");
+
+    useEffect(() => {
+        window.matchMedia("prefers-color-theme: dark").matches ? setTheme("dark") : setTheme("light");
+    }, []);
+    useEffect(() => {
+        isDarkTheme ? document.documentElement.classList.add("dark") : document.documentElement.classList.remove("dark");
+    }, [theme]);
+
     return (
-        <nav>
-            <div className="bg-prheart-200 w-full">
+        <nav className="px-4 bg-prheart-200 w-full flex justify-between">
+            <div>
                 <ul className={(burgerOpen ? "top-12" : "-top-[120px]") + " nav-list"}>
                     <a href="#about-me" className="link">About Me</a>
                     <a href="#projects" className="link">Projects</a>
                     <a href="#contact" className="link">Contacts</a>
                 </ul>
 
-                <button className="md:hidden ml-2 py-3 size-12 flex flex-col flex-nowrap justify-between" onClick={toggleBurger}>
+                <button className="md:hidden py-3 size-12 flex flex-col flex-nowrap justify-between" onClick={toggleBurger}>
                     <Hamburger isOpen={burgerOpen} />
                 </button>
             </div>
             
-            <div className="toggles">
+            <div className="flex items-center z-10">
                 <div className="translate"></div>
-                <div className="light-dark-theme"></div>
+                <button onClick={handleThemeToggle}>
+                    <FontAwesomeIcon icon={isDarkTheme ? faMoon : faSun} size="2xl" className="text-prheart-600" />
+                </button>
+                
             </div>
         </nav>
     );
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
-import Navbar from '../components/Navbar'
-import Intro from '../components/Intro'
-import Projects from '../components/Projects'
-import Contact from '../components/Contact'
-import Footer from '../components/Footer'
+import './App.css';
+import Navbar from '../components/Navbar';
+import Intro from '../components/Intro';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 export default function App() {
 
