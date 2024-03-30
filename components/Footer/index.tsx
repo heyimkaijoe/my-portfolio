ab30e018e191cbbe062017f4cde0ebae216bf0a1
kchsu <kevin3117213@gmail.com>
2024-03-30 13:40:35 +0800

    Modify: max-width limit and destop footer hover effect

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,12 +3,12 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
     return (
-        <footer className="bg-prheart-200 dark:bg-prheart-700 mt-20 py-4 text-prheart-950 dark:text-prheart-50">
+        <footer className="bg-prheart-200 dark:bg-prheart-700 mt-20 py-4 text-prheart-800 dark:text-prheart-200">
             <div className="flex justify-center gap-4 mb-2">
-                <a href="https://github.com/heyimkaijoe">
+                <a href="https://github.com/heyimkaijoe" className="social-link">
                     <FontAwesomeIcon icon={faGithub} size="xl" />
                 </a>
-                <a href="https://www.linkedin.com/in/kchsutwtpe/">
+                <a href="https://www.linkedin.com/in/kchsutwtpe/" className="social-link">
                     <FontAwesomeIcon icon={faLinkedin} size="xl" />
                 </a>
             </div>
