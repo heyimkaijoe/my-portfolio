041642f44234f4c595cd3b4d8f7eff57710a4e54
kchsu <kevin3117213@gmail.com>
2024-04-01 23:53:09 +0800

    Modify: mobile project img height

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -7,7 +7,7 @@ export default function ProjectCard({ name, skills, imgUrl, link }: {
     return (
         <li className="dark-box">
             <a href={link} className="overflow-hidden hover:opacity-80">
-                <img src={imgUrl} alt={name} className="object-cover h-[360px]" />
+                <img src={imgUrl} alt={name} className="object-cover h-[300px] md:h-[360px]" />
                 <div className="md:text-2xl text-left my-3 px-4">{name}</div>
                 <div className="flex gap-2 px-4 pb-4">
                     {skills.map((skill) => <div key={skill} className="card-sm">{skill}</div>)}
