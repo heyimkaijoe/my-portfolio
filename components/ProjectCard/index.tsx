8f79f0d815318487a14afc01891e3846a1c06abe
kchsu <kevin3117213@gmail.com>
2024-04-07 21:48:13 +0800

    Modify: EOL & indent

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -7,8 +7,12 @@ export default function ProjectCard({ name, skills, imgUrl, link }: {
     return (
         <li className="dark-box">
             <a href={link} className="hover:opacity-80">
-                <div className="overflow-hidden pt-4 px-4"><img src={imgUrl} alt={name} className="object-cover rounded-md w-full h-40 md:h-60 lg:h-80"/></div>
+                <div className="overflow-hidden pt-4 px-4">
+                    <img src={imgUrl} alt={name} className="object-cover rounded-md w-full h-40 md:h-60 lg:h-80"/>
+                </div>
+
                 <div className="md:text-2xl text-left my-3 px-4">{name}</div>
+                
                 <div className="flex gap-2 flex-wrap px-4 pb-4">
                     {skills.map((skill) => <div key={skill} className="card-sm">{skill}</div>)}
                 </div>
